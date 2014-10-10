3eb46e7f07771a6ce8f2a9304c1c0a34a70d6f38
Mangirdas Skripka <mangirdas@impresspages.org>
2014-10-10 20:43:09 +0300

    fix error handling in html2text

--- Html2Text.php
+++ Html2Text.php
@@ -56,9 +56,12 @@ class Html2Text
         $html = self::fix_newlines($html);
 
         $doc = new \DOMDocument();
-        if (!$doc->loadHTML($html))
+        $prevValue = libxml_use_internal_errors(true);
+        $loaded = $doc->loadHTML($html);
+        libxml_use_internal_errors($prevValue);
+        if (!$loaded) {
             throw new Html2TextException("Could not load HTML - badly formed?", $html);
-
+        }
         $output = self::iterate_over_node($doc);
 
         // remove leading and trailing spaces on each line
